569a93cb53c885daecba7487ad2f49b133b77d6d
madeinusmate <25027502+madeinusmate@users.noreply.github.com>
2020-08-18 23:38:24 +0800

    update

--- setup.sh
+++ setup.sh
@@ -1,8 +1,11 @@
 mkdir -p ~/.streamlit/
-
+echo "\
+[general]\n\
+email = \"your@domain.com\"\n\
+" > ~/.streamlit/credentials.toml
 echo "\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
-\n\
-“ > ~/.streamlit/config.toml
+port = $PORT\n\
+" > ~/.streamlit/config.toml
